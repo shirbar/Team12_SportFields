1aa1785f25318dfd8db142166a0f635e051671eb
HenDahan <45328465+HenDahan@users.noreply.github.com>
2020-04-06 16:38:27 +0300

    add a login page class

--- app/src/main/java/com/example/team12_sportfields/LoginActivity.java
+++ app/src/main/java/com/example/team12_sportfields/LoginActivity.java
@@ -1,14 +1,86 @@
-package com.example.team12_sportfields;
+package com.example.team_12_lolipop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mEmail, mPassword;
+    Button loginBtn;
+    TextView registerTxt;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        InitializeFields();
     }
+
+    private void InitializeFields() {
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+        registerTxt = findViewById(R.id.registerTxt);
+        progressBar = findViewById(R.id.progressBar);
+
+    }
+
+
+    public void registerClick(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void loginClick(View view){
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            mEmail.setError("Email is Required.");
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            mPassword.setError("Email is Required.");
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Logged in Successfully.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+
+
 }
